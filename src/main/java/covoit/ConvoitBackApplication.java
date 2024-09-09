e09fb9e11b9358db05af3d825691d01e74547afe
MiL-Dev <mil.devacc@gmail.com>
2024-09-09 15:44:48 +0200

    add variable profile appProperties

--- src/main/java/covoit/ConvoitBackApplication.java
+++ src/main/java/covoit/ConvoitBackApplication.java
@@ -1,11 +1,12 @@
 package covoit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConvoitBackApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConvoitBackApplication.class, args);
 	}
